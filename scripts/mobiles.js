32ba1b4ea674dba041cf8f59c329f7f2444543fa
Soumya Chakraborty <soumyachakraborty00@gmail.com>
2022-09-12 00:54:11 +0530

    11092022

---
+++ scripts/mobiles.js
@@ -0,0 +1,68 @@
+const mobiles = [
+	{
+		img: "../img/mobiles/realme/01/01.webp",
+		name: "realme C31 (Light Silver)",
+		storage: "3GB RAM | 32GB ROM | Expandable Upto 1TB",
+		display: "16.56 cm (6.52 inch) HD Display",
+		cammera: "13MP + 2MP + 0.3MP | 5MP Front Camera",
+		battery: "5000 mAh Battery",
+		processor: "Unisoc T612 Processor",
+		warrenty: "1 Year Warranty for Phone and 6 Months Warranty for In-Box Accessories",
+		price: "₹8,299"
+	},
+
+	{
+		img: "../img/mobiles/realme/02/01.webp",
+		name: "realme C31 (Dark Green)",
+		storage: "3GB RAM | 32GB ROM | Expandable Upto 1TB",
+		display: "16.56 cm (6.52 inch) HD Display",
+		cammera: "13MP + 2MP + 0.3MP | 5MP Front Camera",
+		battery: "5000 mAh Battery",
+		processor: "Unisoc T612 Processor",
+		warrenty: "1 Year Warranty for Phone and 6 Months Warranty for In-Box Accessories",
+		price: "₹8,299"
+	},
+
+	{
+		img: "../img/mobiles/realme/03/01.webp",
+		name: "realme C30 (Bamboo Green)",
+		storage: "3GB RAM | 32GB ROM | Expandable Upto 1TB",
+		display: "16.51 cm (6.5 inch) HD+ Display",
+		cammera: "8MP Rear Camera | 5MP Front Camera",
+		battery: "5000 mAh Lithium Ion Battery",
+		processor: "Unisoc T612 Processor",
+		warrenty: "1 Year Manufacturer Warranty for Phone and 6 Months Warranty for In-Box Accessories",
+		price: "₹7,999"
+	},
+
+	{
+		img: "../img/mobiles/realme/04/01.webp",
+		name: "realme 9 (Sunburst Gold)",
+		storage: "6 GB RAM | 128 GB ROM | Expandable Upto 256 GB",
+		display: "16.26 cm (6.4 inch) Full HD+ AMOLED Display",
+		cammera: "108MP + 8MP + 2MP | 16MP Front Camera",
+		battery: "5000 mAh Lithium Ion Battery",
+		processor: "Qualcomm Snapdragon 680 Processor",
+		warrenty: "1 Year Manufacturer Warranty for Phone and 6 Months Warranty for In-Box Accessories",
+		price: "₹14,499"
+	},
+
+	{
+		img: "../img/mobiles/realme/05/01.webp",
+		name: "realme 9 Pro+ 5G (Aurora Green)",
+		storage: "8 GB RAM | 256 GB ROM",
+		display: "16.26 cm (6.4 inch) Full HD+ AMOLED Display",
+		cammera: "50MP + 8MP + 2MP | 16MP Front Camera",
+		battery: "4500 mAh Li-ion Battery",
+		process: "Mediatek Dimensity 920 Processor",
+		warrenty: "1 Year Warranty for Phone and 6 Months Warranty for In-Box Accessories",
+		price: "₹26,999"
+	}
+]
+
+const realmeHolder = document.getElementById("contentDiv")
+let realmeHolderInnerHTML = ""
+
+mobiles.forEach(e => {
+	realmeHolder +=  
+});
